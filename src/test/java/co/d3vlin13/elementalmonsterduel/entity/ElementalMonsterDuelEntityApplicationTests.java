9efd510d173b9cdc9d8aa50a12a13db1b8ef180a
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-08-27 13:54:46 -0500

    Proyect init

---
+++ src/test/java/co/d3vlin13/elementalmonsterduel/entity/ElementalMonsterDuelEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package co.d3vlin13.elementalmonsterduel.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElementalMonsterDuelEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
