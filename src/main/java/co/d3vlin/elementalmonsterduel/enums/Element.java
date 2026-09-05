967dd6cf84b01f2acafe42db5e0bc2fca9b1c05a
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-05 18:16:51 -0500

    chore: Edit element

--- src/main/java/co/d3vlin/elementalmonsterduel/enums/Element.java
+++ src/main/java/co/d3vlin/elementalmonsterduel/enums/Element.java
@@ -1,16 +1,26 @@
 package co.d3vlin.elementalmonsterduel.enums;
 
 public enum Element {
-    EARTH,
-    WATER,
-    AIR,
-    ETHER,
-    VOID,
-    PLASMA,
-    FIRE,
-    SULFUR,
-    FLESH,
-    BLOOD,
-    FUNGI,
-    CARBON
+    EARTH(Group.NATURAL),
+    WATER(Group.NATURAL),
+    AIR(Group.ETHEREAL),
+    ETHER(Group.ETHEREAL),
+    VOID(Group.COSMIC),
+    PLASMA(Group.COSMIC),
+    FIRE(Group.ENERGETIC),
+    SULFUR(Group.ENERGETIC),
+    FLESH(Group.ORGANIC),
+    BLOOD(Group.ORGANIC),
+    FUNGI(Group.MATERIAL),
+    CARBON(Group.MATERIAL);
+
+    private final Group group;
+
+    Element(Group group) {
+        this.group = group;
+    }
+
+    public Group group() {
+        return group;
+    }
 }
