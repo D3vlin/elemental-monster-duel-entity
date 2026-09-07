e499c4553c793923c1e7ce9b6f45d7b5d7c2d28c
Alexis Duque <46509440+D3vlin@users.noreply.github.com>
2026-09-07 05:52:05 -0500

    chore: Add DuelResultCounterEntity

---
+++ src/main/java/co/d3vlin/elementalmonsterduel/entity/DuelResultCounterEntity.java
@@ -0,0 +1,29 @@
+package co.d3vlin.elementalmonsterduel.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(schema = "public", name = "duel_result_counter")
+@Getter
+@Setter
+public class DuelResultCounterEntity {
+
+    @Column(name = "total", nullable = false)
+    private Integer total;
+
+    @Column(name = "wins", nullable = false)
+    private Integer wins;
+
+    @Column(name = "losses", nullable = false)
+    private Integer losses;
+
+    @Column(name = "draws", nullable = false)
+    private Integer draws;
+
+    @Column(name = "abandons", nullable = false)
+    private Integer abandons;
+}
